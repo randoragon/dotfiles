49df8b020289a4f31ae5cc4c149cdba7448fbf03
Randoragon <randoragongamedev@gmail.com>
2019-12-25 14:07:26 +0100

    Enhance rofi playlist entry adding
    I've replaced the simple bash script for appending filenames at the end
    of a playlist with a slightly more sophisticated python script that also
    checks if the given song already exists on the playlist, and informs the
    user of such cases with a rofi message prompt.

---
+++ H_rofi/.config/rofi/playlists_add.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# This script intakes a m3u playlist path, reads the current playing song in MPD,
+# and then appends the song's path to the playlist file IF the song isn't already
+# on the playlist.
+
+import os
+import sys
+import subprocess
+
+def rprint(msg):
+    return os.system('rofi -e "{}" -markup'.format(msg))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        rprint('<i>E: playlists_add.py:</i> <b>Exactly one parameter required (received {})</b>'.format(len(sys.argv)-1))
+        sys.exit(1)
+    
+    playlist = str(sys.argv[1])
+    proc = subprocess.Popen('mpc current -f %file% --port=6601', stdout=subprocess.PIPE, shell=True)
+    song = str(proc.stdout.read())[2:-3]
+
+    f = open(playlist, 'r')
+    if song+'\n' not in f.readlines():
+        f.close()
+        f = open(playlist, 'a')
+        f.write(song)
+        f.close()
+        sys.exit(0)
+    else:
+        rprint('<i>I: playlists_add.py:</i> <b>This song is already on the playlist!</b>')
+        sys.exit(2)

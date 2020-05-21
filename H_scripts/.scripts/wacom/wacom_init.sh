57ad98182e9c711c187793618e0d99577d9d5ee7
Randoragon <randoragongamedev@gmail.com>
2020-05-21 14:01:48 +0200

    Update wacom configuration and make it smart
    Device IDs are now extracted directly from the output of the `xsetwacom
    list devices` command, because they turned out to be changing after
    reboots.
    
    Adjusted some stylus settings to fit my taste better, flipped the tablet
    device by 180 degrees because that's what fits my setup best.
    
    Moved wacom initialization script from xprofile to xinitrc, because
    that's the easiest way to guarantee all environment variables get
    exported properly.

--- H_scripts/.scripts/wacom/wacom_init.sh
+++ H_scripts/.scripts/wacom/wacom_init.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+# This file is sourced in xinitrc to apply
+# some default configuration to a wacom device
+# and also export some variables to allow outside
+# scripts to be more flexible.
 
-# Applies some default configurations that I like
-# on my Wacom drawing tablet.
-
-# Set device IDs (run `xsetwacom list devices` to see)
-stylus=10
-pad=11
-eraser=19
-cursor=20
+# Extract and export device IDs (run `xsetwacom list devices` to see)
+devices="$(xsetwacom list devices)"
+WACOM_STYLUS_ID="$(printf "%s" "$devices" | grep "type: STYLUS" | sed 's/^.*id: //;s/\s*type: .*//')"
+WACOM_PAD_ID="$(printf "%s" "$devices" | grep "type: PAD" | sed 's/^.*id: //;s/\s*type: .*//')"
+WACOM_ERASER_ID="$(printf "%s" "$devices" | grep "type: ERASER" | sed 's/^.*id: //;s/\s*type: .*//')"
+WACOM_CURSOR_ID="$(printf "%s" "$devices" | grep "type: CURSOR" | sed 's/^.*id: //;s/\s*type: .*//')"
+export WACOM_STYLUS_ID WACOM_PAD_ID WACOM_ERASER_ID WACOM_CURSOR_ID
 
 # Set and export desired button IDs
 WACOM_PAD_B1=11
@@ -20,12 +22,21 @@ export WACOM_PAD_B1 WACOM_PAD_B2 WACOM_PAD_B3 WACOM_PAD_B4 WACOM_STYLUS_B1 WACOM
 
 # Remap buttons
 # (check your tablet's button IDs by running xev)
-xsetwacom set "$pad" Button 1 "$WACOM_PAD_B1"
-xsetwacom set "$pad" Button 2 "$WACOM_PAD_B2"
-xsetwacom set "$pad" Button 3 "$WACOM_PAD_B3"
-xsetwacom set "$pad" Button 8 "$WACOM_PAD_B4"
-xsetwacom set "$stylus" Button 2 "$WACOM_STYLUS_B1"
-xsetwacom set "$stylus" Button 3 "$WACOM_STYLUS_B2"
+xsetwacom set "$WACOM_PAD_ID" Button 8 "$WACOM_PAD_B1"
+xsetwacom set "$WACOM_PAD_ID" Button 3 "$WACOM_PAD_B2"
+xsetwacom set "$WACOM_PAD_ID" Button 2 "$WACOM_PAD_B3"
+xsetwacom set "$WACOM_PAD_ID" Button 1 "$WACOM_PAD_B4"
+xsetwacom set "$WACOM_STYLUS_ID" Button 2 "$WACOM_STYLUS_B1"
+xsetwacom set "$WACOM_STYLUS_ID" Button 3 "$WACOM_STYLUS_B2"
 
 # Disable touch feature
-xsetwacom set "$pad" Touch off
+xsetwacom set "$WACOM_PAD_ID" Touch off
+
+# Set absolute mode
+xsetwacom set "$WACOM_STYLUS_ID" Mode Absolute
+
+# Set minimum pen proximity (subjective preference)
+xsetwacom set "$WACOM_STYLUS_ID" CursorProximity 45
+
+# Rotate upside-down (works better with my setup)
+xsetwacom set "$WACOM_STYLUS_ID" Rotate half
--- H_scripts/.scripts/wacom/wacom_pad_b1.sh
+++ H_scripts/.scripts/wacom/wacom_pad_b1.sh
@@ -2,3 +2,8 @@
 
 # This script is executed by xbindkeys when button1 is pressed on a wacom tablet device.
 
+if [ "$(xsetwacom get "$WACOM_STYLUS_ID" Mode)" = Absolute ]; then
+    xsetwacom set "$WACOM_STYLUS_ID" Mode Relative
+else
+    xsetwacom set "$WACOM_STYLUS_ID" Mode Absolute
+fi

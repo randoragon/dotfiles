4ec353589cd745e2a0863349d9d140d3087e0f6b
Randoragon <randoragongamedev@gmail.com>
2021-03-22 19:26:21 +0100

    [ncmpcpp] Improve song change script
    Ncmpcpp will now run the script silently in the background.
    
    New cool feature:
    Automatically have your wallpaper changed to the playing song's cover
    art. See ncmpcpp-song-change.sh script for details.

--- scripts/.scripts/ncmpcpp-song-change.sh
+++ scripts/.scripts/ncmpcpp-song-change.sh
@@ -2,4 +2,21 @@
 
 # This script is run every time ncmpcpp registers a song change.
 
-printf "%s" "$(mpc current --format '%artist% - %title%')    " >~/.cache/obs-now-playing.txt
+# Dump now playing to a text file for other programs to read
+printf "%s" "$(mpc current --format '%artist% - %title%')    " >${XDG_CACHE_HOME:-~/.cache}/ncmpcpp-now-playing.txt
+
+# SET ALBUM COVER AS WALLPAPER
+#
+# Uncomment the line below to activate:
+# switch_wallpaper=1
+if [ -n "$switch_wallpaper" ]; then
+    mroot=~/Music
+    cachefile="${XDG_CACHE_HOME:-~/.cache}/ncmpcpp-albumart.jpg"
+    fpath="${mroot}/$(mpc current --format %file% --port "$MPD_PORT")"
+    [ ! -r "$fpath" ] && exit 1
+    if ffmpeg -y -i "$fpath" "$cachefile"; then
+        xwallpaper --maximize "$cachefile"
+    else
+        xwallpaper --zoom "${XDG_CONFIG_HOME:-~/.config}/wallpaper"
+    fi
+fi

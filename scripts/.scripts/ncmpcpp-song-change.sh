f63d84d20eb1a3c849db0c4bf7c00e8de71f39b5
Randoragon <randoragongamedev@gmail.com>
2021-10-03 18:01:44 +0200

    [scripts] Export more "now playing" information
    The script that's run by ncmpcpp each time a track changes has been
    modified to export more information to text files. I did this because I
    needed it for an OBS scene setup.

--- scripts/.scripts/ncmpcpp-song-change.sh
+++ scripts/.scripts/ncmpcpp-song-change.sh
@@ -3,7 +3,10 @@
 # This script is run every time ncmpcpp registers a song change.
 
 # Dump now playing to a text file for other programs to read
-printf "%s" "$(mpc current --format '%artist% - %title%')    " >${XDG_CACHE_HOME:-~/.cache}/ncmpcpp-now-playing.txt
+printf "%s" "$(mpc current --format '%artist% - %title%    ·')    " >${XDG_CACHE_HOME:-~/.cache}/ncmpcpp-now-playing.txt
+printf "%s" "$(mpc current --format '%title%    ·')    " >${XDG_CACHE_HOME:-~/.cache}/ncmpcpp-now-playing-title.txt
+printf "%s" "$(mpc current --format '%artist%    ·')    " >${XDG_CACHE_HOME:-~/.cache}/ncmpcpp-now-playing-artist.txt
+printf "%s" "$(mpc current --format '%album%    ·')    " >${XDG_CACHE_HOME:-~/.cache}/ncmpcpp-now-playing-album.txt
 
 # SET ALBUM COVER AS WALLPAPER
 #
@@ -17,6 +20,7 @@ if [ -n "$switch_wallpaper" ]; then
     if ffmpeg -y -i "$fpath" "$cachefile"; then
         xwallpaper --maximize "$cachefile"
     else
+        rm -f -- "$cachefile"
         xwallpaper --zoom "${XDG_CONFIG_HOME:-~/.config}/wallpaper"
     fi
 fi

bba2c3c7452ad0ac2513f6eb8b3c809523ae317e
Randoragon <randoragongamedev@gmail.com>
2022-02-21 17:14:43 +0100

    [scripts] Set polybar iowait module interval to 5s
    10 seconds (the former setting) was unnecessarily high and kind of
    annoying i guess?

--- scripts/.scripts/herbstluftwm/bar_iowait.sh
+++ scripts/.scripts/herbstluftwm/bar_iowait.sh
@@ -6,7 +6,7 @@ print () {
     echo " ${4%.*}%"
 }
 
-iostat -c 10 | while read -r line; do
+iostat -c 5 | while read -r line; do
     [ -z "$line" ] && continue
     # shellcheck disable=SC2086
     echo "$line" | grep -q '^[0-9]' && print $line

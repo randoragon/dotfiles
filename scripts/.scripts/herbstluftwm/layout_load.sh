d6a9784f75154fb04423a3bb61f0184376480807
Randoragon <randoragongamedev@gmail.com>
2022-02-28 00:04:07 +0100

    [hlwm] Fix layout_load.sh
    Follow-up to 656d7a15d8fcb8e71a763e8cebafbc947008a0f5
    I forgot to modify slightly the load pipeline.

--- scripts/.scripts/herbstluftwm/layout_load.sh
+++ scripts/.scripts/herbstluftwm/layout_load.sh
@@ -6,8 +6,8 @@ LAYOUT_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/herbstluftwm/layouts"
 
 [ ! -d "$LAYOUT_DIR" ] && exit
 
-name="$(find "$LAYOUT_DIR/" -type f -exec basename {} \; | sort \
-      | dmenu -f -l 10 -p 'Load Layout:' | sed 's/^[0-9]\+\s*//')"
+name="$(find "$LAYOUT_DIR/" -type f -exec basename {} \; \
+      | sort | dmenu -f -l 10 -p 'Load Layout:')"
 [ -z "$name" ] || [ ! -r "$LAYOUT_DIR/$name" ] && exit
 
 herbstclient load "$(cat -- "$LAYOUT_DIR/$name")"

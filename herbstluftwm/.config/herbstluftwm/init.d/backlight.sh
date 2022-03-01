ce34d436ddfb5789ceec7104f5ebe9e885022d0c
Randoragon <randoragongamedev@gmail.com>
2022-03-01 09:51:06 +0100

    [hlwm] Add more backlight keybinds
    On some linux laptops, after resuming from suspend, the screen
    brightness resets back to 100% for some reason. I haven't found a way to
    mitigate this yet, but introducing keybinds for setting the brightness
    to 0% and 100% is a handy workaround.

--- herbstluftwm/.config/herbstluftwm/init.d/backlight.sh
+++ herbstluftwm/.config/herbstluftwm/init.d/backlight.sh
@@ -2,3 +2,5 @@
 
 herbstclient keybind XF86MonBrightnessUp   spawn xbacklight -inc 5
 herbstclient keybind XF86MonBrightnessDown spawn xbacklight -dec 5
+herbstclient keybind Shift+XF86MonBrightnessUp   spawn xbacklight -set 100
+herbstclient keybind Shift+XF86MonBrightnessDown spawn xbacklight -set 0

6075f84460b31cf948a79068d605c01b892292a4
Randoragon <randoragongamedev@gmail.com>
2022-03-01 21:17:26 +0100

    [hlwm] Improve polybar toggle, more bar bindings
    Toggling now actually hides/shows polybar, instead of killing and
    spawning, which was slow and buggy.
    
    New bindings to restart and kill.

--- scripts/.scripts/herbstluftwm/bar_toggle.sh
+++ scripts/.scripts/herbstluftwm/bar_toggle.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-# Kills or spawns the status bar.
-# Spawned status bars are automatically lowered.
+# Hides or shows polybar.
 
 hc () {
     herbstclient "$@"
 }
 
-if pidof -q polybar; then
-    pkill -x polybar
-    hc set_attr monitors.focus.pad_up 0
-else
+pidof -q polybar || {
     hc set_attr monitors.focus.pad_up 20
     ~/.config/polybar/launch.sh &
     wid="$(xdo id -mn polybar)"
@@ -20,4 +16,14 @@ else
     # bar_frame.sh to print an initial state for polybar.
     hc chain , emit_hook bar_workspaces \
              , emit_hook bar_frame
+    exit
+}
+
+if hc compare monitors.focus.pad_up = 0; then
+    polybar-msg cmd show
+    hc set_attr monitors.focus.pad_up 20
+else
+    polybar-msg cmd hide
+    hc set_attr monitors.focus.pad_up 0
+
 fi

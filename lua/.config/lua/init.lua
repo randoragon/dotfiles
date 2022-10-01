1afd4c90e096066abe80286706bdf01d7d8a5cf5
Randoragon <randoragongamedev@gmail.com>
2022-10-01 21:55:47 +0200

    [lua] Update tprint to support table keys

--- lua/.config/lua/init.lua
+++ lua/.config/lua/init.lua
@@ -1,14 +1,20 @@
 -- sources: https://gist.github.com/hashmal/874792
 --          https://gist.github.com/xytis/5361405
 function tprint (tbl, indent)
-  if not indent then indent = 0 end
-  for k, v in pairs(tbl) do
-    formatting = string.rep("  ", indent) .. k .. ": "
-    if type(v) == "table" then
-      print(formatting)
-      tprint(v, indent+1)
-    else
-      print(formatting .. tostring(v))
-    end
-  end
+	indent = indent or 0
+	for k, v in pairs(tbl) do
+		if type(k) ~= 'table' then
+			io.write(string.rep('  ', indent)..k..':')
+		else
+			print(string.rep('  ', indent)..'{')
+			tprint(k, indent + 1)
+			io.write(string.rep('  ', indent)..'}:')
+		end
+		if type(v) == 'table' then
+			print()
+			tprint(v, indent + 1)
+		else
+			print(' '..tostring(v))
+		end
+	end
 end

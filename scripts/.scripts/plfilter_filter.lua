fae32b6ceb13ee86c0217956ea03428a1de73fb1
Randoragon <randoragongamedev@gmail.com>
2023-05-19 00:38:17 +0200

    [scripts] plfilter: Return all on empty query
    Also changed the lua script usage: now it no longer accepts a file path
    argument; it simply reads lines from stdin. This is more universal.

--- scripts/.scripts/plfilter_filter.lua
+++ scripts/.scripts/plfilter_filter.lua
@@ -1,26 +1,28 @@
 #!/usr/bin/env lua
 
 -- Supplementary script for plfilter.
--- Intakes a path to a text file of the form:
+-- Reads from stdin input of the form:
 --      +playlist1
 --      +playlist2
 --      -playlist3
 --      +playlist4
 --      ...
--- Prints out a filtered playlist in which all songs simultaneously belong
--- to all '+' playlists and don't belong to any of '-' playlists.
+-- Prints out a filtered playlist in which all songs simultaneously belong to
+-- all '+' playlists and don't belong to any of '-' playlists.
+-- In case of an empty file, all files in the music library will be returned
+-- (this requires mpc installed and an up-to-date MPD database).
 
-MUSICDIR = os.getenv('HOME')..'/Music/'
+MUSIC_DIR = os.getenv('HOME')..'/Music/'
 hashset = {}
 isfirst = true
-for playlist in io.lines(arg[1]) do
+for playlist in io.stdin:lines() do
     if isfirst then
         if playlist:sub(1, 1) == '+' then
             for song in io.lines(playlist:sub(2)) do
                 hashset[song] = true
             end
         else
-            local f = assert(io.popen(("cd '%s' && find . -type f -name '*.mp3'"):format(MUSICDIR), 'r'))
+            local f = assert(io.popen(("cd '%s' && find . -type f -name '*.mp3'"):format(MUSIC_DIR), 'r'))
             local output = f:read('*a')
             f:close()
             for song in output:gmatch('[^\n]*') do
@@ -46,6 +48,9 @@ for playlist in io.lines(arg[1]) do
             end
         end
     end
+end
+if isfirst then
+	os.execute(("mpc listall"))
 end
 for k, _ in pairs(hashset) do
     print(k)

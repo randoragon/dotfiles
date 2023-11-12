25b2ade721779ba5a8dc8a4738f96789bbd3dead
Randoragon <randoragongamedev@gmail.com>
2023-11-12 02:49:27 +0100

    [nvim] lua: Refactor mappings.lua

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,13 +1,13 @@
-vim.g.mapleader = ','
-
 local map = vim.keymap.set
+
+vim.g.mapleader = ','
 
 -- Miscellaneous
 map("n", "<Leader>w", ":set wrap! linebreak!<CR>")
 map("n", "Y", "y$", { remap=true })
 map("n", "c", '"_c')
-map("v", ".", ":normal .<CR>", { silent=true })
-map("v", "<Leader>/", ":nohlsearch<CR>", { silent=true })
+map("v", ".", function() vim.cmd.normal(".") end, { silent=true })
+map("v", "<Leader>/", vim.cmd.nohlsearch, { silent=true })
 map("n", "<PageUp>", "<C-u>", { remap=true })
 map("n", "<PageDown>", "<C-d>", { remap=true })
 
@@ -16,9 +16,9 @@ map("n", "<Leader>.L", lsp_toggle_project_mode, { silent=true })
 map("n", "<Space>", "<Nop>")
 
 -- Quickfix/location lists
-map("n", "<Leader>q", ":ToggleCList<CR>", { silent=true })
-map("n", "<Leader>d", ":ToggleLList<CR>", { silent=true })
-map("n", "[q", ":Cprev<CR>", { silent=true })
-map("n", "]q", ":Cnext<CR>", { silent=true })
-map("n", "[d", ":Lprev<CR>", { silent=true })
-map("n", "]d", ":Lnext<CR>", { silent=true })
+map("n", "<Leader>q", vim.cmd.ToggleCList, { silent=true })
+map("n", "<Leader>d", vim.cmd.ToggleLList, { silent=true })
+map("n", "[q", vim.cmd.Cprev, { silent=true })
+map("n", "]q", vim.cmd.Cnext, { silent=true })
+map("n", "[d", vim.cmd.Lprev, { silent=true })
+map("n", "]d", vim.cmd.Lnext, { silent=true })

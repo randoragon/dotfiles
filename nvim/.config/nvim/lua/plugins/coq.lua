39cecc2ad14504b95e55d9b19a9ea3c81e0537cc
Randoragon <randoragongamedev@gmail.com>
2023-11-28 00:20:48 +0100

    [nvim] Configure nvim_coq plugin

--- nvim/.config/nvim/lua/plugins/coq.lua
+++ nvim/.config/nvim/lua/plugins/coq.lua
@@ -2,4 +2,17 @@
 vim.g.coq_settings = {
 	auto_start = true,
 	xdg = true,
+	["display.statusline.helo"] = false,
+
+	clients = {
+		snippets = {
+			enabled = false,
+			warn = {},
+			user_path = os.getenv("XDG_CONFIG_HOME") .. "/nvim/lua/plugins/coq-user-snippets/",
+		},
+	},
+
+	keymap = {
+		manual_complete_insertion_only = true,
+	},
 }

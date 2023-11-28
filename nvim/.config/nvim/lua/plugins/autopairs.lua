98a537d8d0c0c9dc591520374c5160fee2e8f173
Randoragon <randoragongamedev@gmail.com>
2023-11-28 15:39:03 +0100

    [nvim] Fix coq/autopairs conflict

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -4,7 +4,7 @@ local rule = require("nvim-autopairs.rule")
 local cond = require("nvim-autopairs.conds")
 
 autopairs.setup({
-	disable_filetype = { "TelescopePrompt", "spectre_panel" },
+	disable_filetype = {"TelescopePrompt", "spectre_panel"},
 	disable_in_macro = true,
 	disable_in_visualblock = false,
 	disable_in_replace_mode = true,
@@ -22,13 +22,38 @@ autopairs.setup({
 		keys = "qwertyuiopzxcvbnmasdfghjkl",
 		check_comma = true,
 		highlight = "Search",
-		highlight_grey="Comment"
+		highlight_grey = "Comment"
 	},
 	break_undo = true,
 	check_ts = true,
-	map_cr = true,
-	map_bs = true,
+	map_cr = false,  -- See mappings.lua
+	map_bs = false,  -- See mappings.lua
 	map_c_h = false,
 	map_c_w = false,
 })
 
+-- Fix nvim-autopairs and nvim_coq conflicting mappings
+-- NOTE: for some reason, using vim.keymap.set() does not work for these.
+function autopairs_coq_cr()
+	if vim.fn.pumvisible() ~= 0 then
+		if vim.fn.complete_info({"selected"}).selected ~= -1 then
+			-- Completion menu is open and something is selected
+			return autopairs.esc("<C-y>")
+		else
+			-- Completion menu is open, but nothing is selected
+			return autopairs.esc("<C-e>") .. autopairs.autopairs_cr()
+		end
+	else
+		return autopairs.autopairs_cr()
+	end
+end
+function autopairs_coq_bs()
+	if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({"selected"}).selected ~= -1 then
+		-- Completion menu is open and something is selected
+		return autopairs.esc("<C-y>") .. autopairs.autopairs_bs()
+	else
+		return autopairs.autopairs_bs()
+	end
+end
+vim.api.nvim_set_keymap("i", "<CR>", "v:lua.autopairs_coq_cr()", {expr=true, noremap=true})
+vim.api.nvim_set_keymap("i", "<BS>", "v:lua.autopairs_coq_bs()", {expr=true, noremap=true})
--- nvim/.config/nvim/lua/plugins/coq.lua
+++ nvim/.config/nvim/lua/plugins/coq.lua
@@ -13,6 +13,21 @@ vim.g.coq_settings = {
 	},
 
 	keymap = {
+		recommended = false,
 		manual_complete_insertion_only = true,
 	},
 }
+
+-- Default keymaps, except CR and BS (needs special handling due to autopairs)
+local function ifelsepumvisible(iftrue, iffalse)
+	return function()
+		if vim.fn.pumvisible() ~= 0 then
+			return vim.api.nvim_replace_termcodes(iftrue, true, false, true)
+		end
+		return vim.api.nvim_replace_termcodes(iffalse, true, false, true)
+	end
+end
+map("i", "<Esc>", ifelsepumvisible("<C-e><Esc>", "<Esc>"), {expr=true})
+map("i", "<C-c>", ifelsepumvisible("<C-e><C-c>", "<C-c>"), {expr=true})
+map("i", "<Tab>", ifelsepumvisible("<C-n>", "<Tab>"), {expr=true})
+map("i", "<S-Tab>", ifelsepumvisible("<C-p>", "<BS>"), {expr=true})

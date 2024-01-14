3edb8466a17bdaeacaf9788c31b48740e19b3186
Randoragon <randoragongamedev@gmail.com>
2024-01-14 11:58:25 +0100

    [spectrwm] Add gpg_expire bar display
    Reports the amount of time left until the nearest GPG private key
    expiration date.

--- scripts/.scripts/spectrwm/bar_action.sh
+++ scripts/.scripts/spectrwm/bar_action.sh
@@ -1,15 +1,35 @@
 #!/bin/sh
 
+datetime () {
+    date +'%a %m/%d  %H:%M:+@fn=2;%S+@fn=0;'
+}
+
+gpg_expire () {
+    epoch_expire=$(($(gpg -K --with-colons | grep '^\(sec\|ssb\)' | cut -d: -f7 | sort -n | head -n1)))
+    epoch_now=$(($(date +%s)))
+    if [ $epoch_now -ge $epoch_expire ]; then
+        echo EXPIRED
+    else
+        n_left=$(((epoch_expire - epoch_now) / 3600))
+        if [ $n_left -eq 0 ]; then
+            echo '<1 hour'
+            return
+        elif [ $n_left -lt 24 ]; then
+            printf '%u hour' "$n_left"
+        else
+            n_left=$((n_left / 24))
+            printf '%u day' "$n_left"
+        fi
+        [ $n_left -ne 1 ] && printf s
+    fi
+}
+
 storage () {
     rootfs=$((1024 * $(df --output=avail / | tail -n1)))
     homefs=$((1024 * $(df --output=avail /home | tail -n1)))
     printf '/ %s  ~ %s\n' \
         "$(numfmt --to iec --format %.1f "$rootfs")" \
         "$(numfmt --to iec --format %.1f "$homefs")"
-}
-
-datetime () {
-    date +'%a %m/%d  %H:%M:+@fn=2;%S+@fn=0;'
 }
 
 volume () {
@@ -23,6 +43,10 @@ volume () {
 
 i=0
 while true; do
+    [ $((i % (4 * 3600))) -eq 0 ] && {
+        gpg_expire="$(gpg_expire)"
+    }
+
     [ $((i % (4 * 10))) -eq 0 ] && {
         storage="$(storage)"
     }
@@ -33,7 +57,7 @@ while true; do
 
     volume="$(volume)"
 
-    printf '%s │ %s │ %s\n' "$volume" "$storage" "$datetime"
+    printf '%s │ %s │ %s │ %s\n' "$volume" "$storage" "$gpg_expire" "$datetime"
 
     i=$((i + 1))
     sleep 0.25

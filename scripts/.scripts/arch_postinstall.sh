564febf9af3782a1e576f94d3ca081cb732b53c0
randoragon <randoragongamedev@gmail.com>
2024-07-04 15:48:29 +0200

    [scripts] Add bemenu renderers to api
    I didn't know they were required until invoking bemenu on wayland
    without bemenu-wayland installed greeted me with a 1 error code.

--- scripts/.scripts/arch_postinstall.sh
+++ scripts/.scripts/arch_postinstall.sh
@@ -180,7 +180,7 @@ done
 
 [ -n "$need_gui" ] && for package in \
     river xdg-desktop-portal xdg-desktop-portal-wlr \
-    swayidle waylock bemenu \
+    swayidle waylock bemenu bemenu-ncurses bemenu-wayland \
     foot \
     grim slurp swappy \
     brightnessctl \

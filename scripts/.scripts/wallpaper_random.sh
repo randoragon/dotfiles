1955b36285a2b2184108375e4031f8f31b28de1a
randoragon <randoragongamedev@gmail.com>
2024-07-04 16:47:25 +0200

    [scripts] Disable transitions in wallpaper_random.sh

--- scripts/.scripts/wallpaper_random.sh
+++ scripts/.scripts/wallpaper_random.sh
@@ -4,4 +4,4 @@ WALLPAPER_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/wallpapers"
 
 pick="$(find "$WALLPAPER_DIR" -type f | shuf -n 1)"
 cp -- "$pick" ~/.config/wallpaper
-swww img ~/.config/wallpaper
+swww img -t none ~/.config/wallpaper

e51d672ab4ab050c7b9c2a539fa6d3c7d70a1b6a
randoragon <randoragongamedev@gmail.com>
2024-07-27 21:06:22 +0200

    [river] Set wallpaper at init
    Whoops forgot

--- river/.config/river/init.d/0-wallpaper.sh
+++ river/.config/river/init.d/0-wallpaper.sh
@@ -4,3 +4,5 @@ swww-daemon --format xrgb --no-cache &
 
 riverctl map normal Super+Control+Shift Slash spawn 'swww clear 111111'
 riverctl map normal Super+Control Slash spawn '~/.scripts/wallpaper_random.sh'
+
+swww img -t none ~/.config/wallpaper
